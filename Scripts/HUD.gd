605900a46953667bb8ad70a79c68726ba9a74dfa
Niclas Eriksen <niclaseriksen@gmx.com>
2016-12-26 22:20:54 +0100

    Ability bar, UI themes

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -1,13 +1,8 @@
 extends CanvasLayer
 
 var hp_bar_scn = preload("res://Scenes/UI/HPBar.tscn")
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
 
 func add_hpbar(owner):
