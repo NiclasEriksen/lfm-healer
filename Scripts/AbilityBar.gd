61026ac1a1e9cd83bff6e8ea49be95fdec4aa5ee
Niclas Eriksen <niclaseriksen@gmx.com>
2016-12-29 02:05:02 +0100

    Panel styling (shitty), ability par tied to screen edge.

--- Scripts/AbilityBar.gd
+++ Scripts/AbilityBar.gd
@@ -3,6 +3,9 @@ onready var game = get_tree().get_root().get_node("Game")
 
 
 func _ready():
+	set_focus_mode(0)
+	for c in get_children():
+		c.set_focus_mode(0)
 	set_process_input(true)
 	set_process(true)
 
