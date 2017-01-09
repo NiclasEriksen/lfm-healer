3e53a88a0d114682c1d331d68b09d140459305fa
Niclas Eriksen <niclaseriksen@gmx.com>
2017-01-09 14:38:38 +0100

    Added template for custom Actor script also.

--- Scenes/Actors/ActorTemplate.gd
+++ Scenes/Actors/ActorTemplate.gd
@@ -1,12 +1,11 @@
 extends "res://Scripts/Actor.gd"
 
-
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
 
 
-
-func _on_ActorBase_attack(target):
-	fire_projectile(target)
+### Override attack method by uncommenting this.
+#func _on_ActorBase_attack(target):
+#	pass
