25799fec9a920916c54ba908f6a5f59aeaf2592e
Niclas Eriksen <niclaseriksen@gmx.com>
2017-01-11 13:01:12 +0100

    Fixed crash on victory, added returns in game.process.

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -56,11 +56,12 @@ func _process(dt):
 	if not friendlies.size():
 		print("All friendly players dead, restarting.")
 		newgame()
-	else:
-		var enemies = get_tree().get_nodes_in_group("enemy")
-		if not enemies.size():
-			print("All enemy players dead, restarting.")
-			newgame()
+		return
+	var enemies = get_tree().get_nodes_in_group("enemy")
+	if not enemies.size():
+		print("All enemy players dead, restarting.")
+		newgame()
+		return
 
 	for f in friendlies:
 		f.get_node("ActorBase").get_node("Selected").set_enabled(false)
