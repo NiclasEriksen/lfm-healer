9daa1967f6ec5d53d83391303c54568dd950572b
Niclas Eriksen <niclaseriksen@gmx.com>
2017-01-16 16:10:17 +0100

    First steps in refactoring how EffectModules behave and interract.

--- Scripts/EffectModule.gd
+++ Scripts/EffectModule.gd
@@ -1,4 +1,4 @@
-extends Node
+extends "res://Scripts/EffectBase.gd"
 
 export(String, "physical", "fire", "water", "life", "earth", "death", "stun") var effect_type = "physical"
 export(String, "hp", "mp", "movement_speed") var effect_stat = "hp"
