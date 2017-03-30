12c2a12f329bcfdadb4901ed87e2b64266955fb2
Niclas Eriksen <niclaseriksen@gmx.com>
2017-03-31 01:05:49 +0200

    Moved shader to HUD layer, made it easy to change render size

---
+++ Scenes/ShaderLayer.gd
@@ -0,0 +1,24 @@
+tool
+extends Node2D
+
+export(Vector2) var size = Vector2(512, 288) setget set_size, get_size
+
+func set_size(s):
+	size = s
+	if not get_tree():
+		return
+	if get_tree().is_editor_hint():
+		get_node("TextureFrame").set_size(s)
+		var m = get_node("TextureFrame").get_material()
+		m.set_shader_param("screen_width", s.x)
+		m.set_shader_param("screen_height", s.y)
+
+func get_size():
+	return size
+
+func _ready():
+	if get_tree().is_editor_hint():
+		hide()
+	else:
+		show()
+
